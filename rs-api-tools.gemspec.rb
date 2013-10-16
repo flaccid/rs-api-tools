06109c634665c74b21050627ffeabffe9fd3c9d8
Chris Fordham <chris@xhost.com.au>
2013-10-16 22:08:11 +1100

    Bump date, update email, add octokit as dep to gemspec.

--- rs-api-tools.gemspec.rb
+++ rs-api-tools.gemspec.rb
@@ -1,16 +1,17 @@
 Gem::Specification.new do |s|
   s.name        = 'rs-api-tools'
   s.version     = '0.0.3'
-  s.date        = '2013-06-12'
+  s.date        = '2013-10-06'
   s.summary     = "rs-api-tools"
   s.description = "RightScale API Command Line Tools."
   s.authors     = ["Chris Fordham"]
-  s.email       = 'chris@xhost.com.au'
+  s.email       = 'chris@fordham-nagy.id.au'
   s.bindir      = 'bin'
   s.executables = Dir.entries(s.bindir) - [".", "..", '.gitignore']
   s.homepage    =
     'https://github.com/flaccid/rs-api-tools'
-  s.add_dependency "json", ">= 1.4.4", "<= 1.6.1"
+  s.add_dependency "json", ">= 1.4.4"
   s.add_dependency 'rest_connection'
   s.add_dependency 'right_api_client'
+  s.add_dependency 'octokit'
 end
