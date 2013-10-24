2ca595ba795663187210cbb92ed994b1d3df7f54
Chris Fordham <chris@xhost.com.au>
2013-10-24 17:47:41 +1100

    Update gemspec for 0.0.5.

--- rs-api-tools.gemspec.rb
+++ rs-api-tools.gemspec.rb
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'rs-api-tools'
-  s.version     = '0.0.4'
-  s.date        = '2013-10-18'
+  s.version     = '0.0.5'
+  s.date        = '2013-10-21'
   s.summary     = "rs-api-tools"
   s.description = "RightScale API Command Line Tools."
   s.authors     = ["Chris Fordham"]
