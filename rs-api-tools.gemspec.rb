c621216a89d44f7114e094c556a1ee840b4035e5
Chris Fordham <chris@xhost.com.au>
2013-10-25 15:10:36 +1100

    Bump gemspec to 0.0.6.

--- rs-api-tools.gemspec.rb
+++ rs-api-tools.gemspec.rb
@@ -1,7 +1,7 @@
 Gem::Specification.new do |s|
   s.name        = 'rs-api-tools'
-  s.version     = '0.0.5'
-  s.date        = '2013-10-21'
+  s.version     = '0.0.6'
+  s.date        = '2013-10-25'
   s.summary     = "rs-api-tools"
   s.description = "RightScale API Command Line Tools."
   s.authors     = ["Chris Fordham"]
